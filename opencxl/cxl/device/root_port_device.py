959f5ed2d356ffef1d540e59c1595406ca78130b
Kyeyoon Park <kyeyoon.park@eeum.io>
2024-06-30 15:03:14 -0500

    file transfer
    Signed-off-by: Kyeyoon Park <kyeyoon.park@eeum.io>

--- opencxl/cxl/device/root_port_device.py
+++ opencxl/cxl/device/root_port_device.py
@@ -308,9 +308,9 @@ class CxlRootPortDevice(RunnableComponent):
             return None
 
     async def cxl_mem_write(self, address: int, data: int) -> int:
-        logger.info(
-            self._create_message(f"CXL.mem Write: HPA addr:0x{address:08x} data:0x{data:08x}")
-        )
+        # logger.info(
+        #     self._create_message(f"CXL.mem Write: HPA addr:0x{address:08x} data:0x{data:08x}")
+        # )
         packet = CxlMemMemWrPacket.create(address, data)
         await self._downstream_connection.cxl_mem_fifo.host_to_target.put(packet)
         try:
